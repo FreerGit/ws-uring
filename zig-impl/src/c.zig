2159c740762368d60a492786e226c0497774428c
sven <Fredrik-98@live.se>
2024-07-19 21:21:06 +0000

    refactor: yeet the zig implementation
    - RIP

--- zig-impl/src/c.zig
+++
@@ -1,6 +0,0 @@
-pub usingnamespace @cImport({
-    @cDefine("_POSIX_C_SOURCE", "200809L");
-    @cInclude("wolfssl/options.h");
-    @cInclude("wolfssl/ssl.h");
-    @cInclude("wolfssl/wolfio.h");
-});
